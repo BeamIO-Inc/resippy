5e58a0a751062d44d07219e84c933d38693b438a
Ryan <ryanlaclair@gmail.com>
2019-07-19 14:34:41 -0400

    Removed default values for parameters in nav factory

--- resippy/photogrammetry/nav/nav_factory.py
+++ resippy/photogrammetry/nav/nav_factory.py
@@ -4,11 +4,11 @@ from resippy.photogrammetry.nav.applanix_sbet_nav import ApplanixSBETNav
 class NavFactory:
 
     @staticmethod
-    def from_applanix_sbet_file(filename,       # type: str
-                                proj='utm',     # type: str
-                                zone=None,      # type: str
-                                ellps='WGS84',  # type: str
-                                datum='WGS84'   # type: str
+    def from_applanix_sbet_file(filename,   # type: str
+                                proj,       # type: str
+                                zone,       # type: str
+                                ellps,      # type: str
+                                datum       # type: str
                                 ):          # type: (...) -> ApplanixSBETNav
         applanix_sbet_nav = ApplanixSBETNav()
         applanix_sbet_nav.load_from_file(filename, proj, zone, ellps, datum)
