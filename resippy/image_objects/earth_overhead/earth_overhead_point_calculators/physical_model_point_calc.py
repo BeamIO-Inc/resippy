5b044e5a6cd54223f5685ece6a93fe6205b3ea68
Ryan LaClair <ryanlaclair@gmail.com>
2019-08-06 14:23:04 -0400

    Stubbed out physical model point calc

--- resippy/image_objects/earth_overhead/earth_overhead_point_calculators/physical_model_point_calc.py
+++ resippy/image_objects/earth_overhead/earth_overhead_point_calculators/physical_model_point_calc.py
@@ -1,1 +1,35 @@
-class PhysicalModelPo
+from resippy.image_objects.earth_overhead.earth_overhead_point_calculators.pinhole_camera_point_calc \
+    import PinholeCameraPointCalc
+
+import numpy as np
+
+
+class PhysicalModelPointCalc(PinholeCameraPointCalc):
+
+    def __init__(self):
+        super(PhysicalModelPointCalc, self).__init__()
+
+        # TODO: initialize distortion model
+        # TODO: initialize exterior orientation
+
+    @classmethod
+    def init_from_coeffs(cls
+                         ):     # type: (...) -> PhysicalModelPointCalc
+        # TODO
+        pass
+
+    @classmethod
+    def init_from_file(cls,
+                       filename     # type: str
+                       ):           # type: (...) -> PhysicalModelPointCalc
+        # TODO
+        pass
+
+    def _lon_lat_alt_to_pixel_x_y_native(self,
+                                         lons,          # type: np.ndarray
+                                         lats,          # type: np.ndarray
+                                         alts=None,     # type: np.ndarray
+                                         band=None      # type: int
+                                         ):             # type: (...) -> (np.ndarray, np.ndarray)
+        # TODO
+        pass
