d912383417f7492fc39443e5e7a8b1af78178450
Ryan LaClair <ryanlaclair@gmail.com>
2019-08-07 11:59:06 -0400

    Moved micasense pix4d point calc functionality to sensor model

--- resippy/image_objects/earth_overhead/earth_overhead_point_calculators/earth_overhead_sensor_model.py
+++ resippy/image_objects/earth_overhead/earth_overhead_point_calculators/earth_overhead_sensor_model.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pyproj import Proj
 
 from resippy.image_objects.earth_overhead.earth_overhead_point_calculators.abstract_earth_overhead_point_calc \
     import AbstractEarthOverheadPointCalc
@@ -31,6 +32,17 @@ class EarthOverheadSensorModel(AbstractEarthOverheadPointCalc):
             return self._point_calcs[band]._pixel_x_y_alt_to_lon_lat_native(pixel_xs, pixel_ys, alts, band)
 
         # TODO: error for band out of range
+
+    def set_projection(self,
+                       projection   # type: Proj
+                       ):           # type: (...) -> None
+        super().set_projection(projection)
+        [point_calc.set_projection(projection) for point_calc in self._point_calcs]
+
+    def set_distortion_model(self,
+                             distortion_model   # type: str
+                             ):                 # type: (...) -> None
+        [point_calc.set_distortion_model(distortion_model) for point_calc in self._point_calcs]
 
     def set_point_calcs(self,
                         point_calcs     # type: [AbstractEarthOverheadPointCalc]
