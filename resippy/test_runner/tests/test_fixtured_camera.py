53b09d490f95c4d5a35df39e5cfd434b6809a616
Jason Casey <j.casey@beamio.net>
2019-09-18 14:46:44 -0400

    adding more unit tests for fixtured camera (again)

--- resippy/test_runner/tests/test_fixtured_camera.py
+++ resippy/test_runner/tests/test_fixtured_camera.py
@@ -186,7 +186,8 @@ class TestFixturedCamera(unittest.TestCase):
 
         assert np.isclose(fixture_x, camera_x)
         assert camera_z > fixture_z
-        assert camera_y > fixture_y
+        assert camera_y < (fixture_y+1)
+        assert np.isclose(np.square(camera_y - fixture_y) + np.square(camera_z - fixture_z), 1.0)
         assert np.isclose(fpa_center_lon, camera_x)
         assert fpa_center_lat > camera_y
 
@@ -194,6 +195,112 @@ class TestFixturedCamera(unittest.TestCase):
         print("makes sense by right-hand-rule conventions.")
         print("a positive y lever arm offset with a positive roll results in the camera moving higher in altitude and lower in latitude.")
         print("test passed.")
+
+    def test_x_lever_arm_offset(self):
+        lat = 43.085898
+        lon = -77.677624
+        sensor_alt = 100
+
+        temp_point_calc = IdealPinholeFpaLocalUtmPointCalc.init_from_wgs84_params(lon, lat, sensor_alt,
+                                                                                 0, 0, 0,
+                                                                                 1000, 1000,
+                                                                                 5, 5, 1)
+
+        fixture_local_lon, fixture_local_lat = temp_point_calc.get_approximate_lon_lat_center()
+
+        fixtured_cam = FixturedCamera()
+        fixtured_cam.set_fixture_xyz(fixture_local_lon, fixture_local_lat, sensor_alt)
+        fixtured_cam.set_relative_camera_xyz(1, 0, 0)
+        fixtured_cam.set_fixture_orientation_by_roll_pitch_yaw(0, 10, 0,
+                                                               roll_units='degrees',
+                                                               pitch_units='degrees',
+                                                               yaw_units='degrees')
+        fixtured_cam.set_boresight_matrix_from_camera_relative_rpy_params(0, 0, 0)
+        camera_M = fixtured_cam.get_camera_absolute_M_matrix()
+        camera_x, camera_y, camera_z = fixtured_cam.get_camera_absolute_xyz()
+
+        omega, phi, kappa = photogram_utils.solve_for_omega_phi_kappa(camera_M)
+        pinhole_cam = PinholeCamera()
+        pinhole_cam.init_pinhole_from_coeffs(camera_x, camera_y, camera_z, omega, phi, kappa,
+                                             1, focal_length_units='meters')
+
+        fpa_point_calc = IdealPinholeFpaLocalUtmPointCalc.init_from_local_params(camera_x, camera_y, camera_z,
+                                                                                 temp_point_calc.get_projection(),
+                                                                                 omega, phi, kappa,
+                                                                                 1000, 1000,
+                                                                                 5, 5, 1)
+
+        fpa_center_lon, fpa_center_lat = fpa_point_calc.pixel_x_y_alt_to_lon_lat(500, 500, 0)
+
+        fixture_x, fixture_y, fixture_z = fixtured_cam.get_fixture_xyz()
+
+        assert np.isclose(fixture_y, camera_y)
+        assert camera_z < fixture_z
+        assert camera_x < (fixture_x+1)
+        assert np.isclose(np.square(camera_x - fixture_x) + np.square(camera_z - fixture_z), 1.0)
+        assert np.isclose(fpa_center_lat, camera_y)
+        assert fpa_center_lon < camera_x
+
+        print("positive pitch results in camera center pointing to a higher latitude")
+        print("makes sense by right-hand-rule conventions.")
+        print("a positive y lever arm offset with a positive roll results in the camera moving higher in altitude and lower in latitude.")
+        print("test passed.")
+
+    def test_x_lever_arm_offset_yaw(self):
+        lat = 43.085898
+        lon = -77.677624
+        sensor_alt = 100
+
+        temp_point_calc = IdealPinholeFpaLocalUtmPointCalc.init_from_wgs84_params(lon, lat, sensor_alt,
+                                                                                 0, 0, 0,
+                                                                                 1000, 1000,
+                                                                                 5, 5, 1)
+
+        fixture_local_lon, fixture_local_lat = temp_point_calc.get_approximate_lon_lat_center()
+
+        fixtured_cam = FixturedCamera()
+        fixtured_cam.set_fixture_xyz(fixture_local_lon, fixture_local_lat, sensor_alt)
+        fixtured_cam.set_relative_camera_xyz(1, 0, 0)
+        fixtured_cam.set_fixture_orientation_by_roll_pitch_yaw(0, 0, 10,
+                                                               roll_units='degrees',
+                                                               pitch_units='degrees',
+                                                               yaw_units='degrees')
+        fixtured_cam.set_boresight_matrix_from_camera_relative_rpy_params(0, 0, 0)
+        camera_M = fixtured_cam.get_camera_absolute_M_matrix()
+        camera_x, camera_y, camera_z = fixtured_cam.get_camera_absolute_xyz()
+
+        omega, phi, kappa = photogram_utils.solve_for_omega_phi_kappa(camera_M)
+        pinhole_cam = PinholeCamera()
+        pinhole_cam.init_pinhole_from_coeffs(camera_x, camera_y, camera_z, omega, phi, kappa,
+                                             1, focal_length_units='meters')
+
+        fpa_point_calc1 = IdealPinholeFpaLocalUtmPointCalc.init_from_local_params(camera_x, camera_y, camera_z,
+                                                                                 temp_point_calc.get_projection(),
+                                                                                 omega, phi, 0,
+                                                                                 1000, 1000,
+                                                                                 5, 5, 1)
+
+        fpa_point_calc2 = IdealPinholeFpaLocalUtmPointCalc.init_from_local_params(camera_x, camera_y, camera_z,
+                                                                                 temp_point_calc.get_projection(),
+                                                                                 omega, phi, kappa,
+                                                                                 1000, 1000,
+                                                                                 5, 5, 1)
+
+        fpa_lon_1, fpa_lat_1 = fpa_point_calc1.pixel_x_y_alt_to_lon_lat(1000, 500, 0)
+        fpa_lon_2, fpa_lat_2 = fpa_point_calc2.pixel_x_y_alt_to_lon_lat(1000, 500, 0)
+
+        fixture_x, fixture_y, fixture_z = fixtured_cam.get_fixture_xyz()
+
+        assert np.isclose(fpa_lat_1, camera_y)
+        assert fpa_lon_1 > camera_x
+        print("last pixel in the array is in the positive longitude direction at roll=0, pitch=0, yaw=0")
+        assert fpa_lon_2 < fpa_lon_1
+        assert fpa_lat_2 > fpa_lat_1
+        print("a positive yaw rotation results seem correct from the perspective of pixel shift")
+        assert camera_x < (fixture_x + 1)
+        assert camera_y > fixture_y
+        print("a positive yaw moves a camera originally located at (1, 0, 0) relative to the fixture to a greater y location and lower x location.")
+        print("yaw rotation tests passed.")
 
 
 if __name__ == '__main__':
